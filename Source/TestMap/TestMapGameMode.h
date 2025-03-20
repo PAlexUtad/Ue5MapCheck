a891cb1af08538e83ca99790a6152d407858021e
PAlexUtad <pages.alexandre@live.u-tad.com>
2025-03-20 18:56:57 +0100

    Added project

---
+++ Source/TestMap/TestMapGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "TestMapGameMode.generated.h"
+
+UCLASS(minimalapi)
+class ATestMapGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	ATestMapGameMode();
+};
+
+
+
